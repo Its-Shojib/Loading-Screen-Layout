6bc1cd08608d96ea99ff80deb6851d088473826d
Md Shojib Hossain <mdshojib922@gmail.com>
2024-02-21 20:04:54 +0600

    Skeleton Bg Changed

--- src/SkeletonCard.jsx
+++ src/SkeletonCard.jsx
@@ -2,8 +2,8 @@ import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 const SkeletonCard = () => {
     return (
-        <div className="card w-96 h-96 bg-base-200 shadow-xl">
-            <SkeletonTheme baseColor="#606060" highlightColor="#222">
+        <div className="card w-96 h-96 bg-gray-600 shadow-xl">
+            <SkeletonTheme baseColor="#606060" highlightColor="#121212">
               <figure className="px-10 py-10">
                 <Skeleton circle={true} height={150} width={150}></Skeleton>
               </figure>
